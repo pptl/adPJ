90d2f7b7327f9e787165c88b49f301747d2fa548
brianfung420 <brianfung420@gmail.com>
2019-06-09 14:10:33 +0800

    add EventRepository

---
+++ androidFile/app/src/main/java/com/example/database/EventRepository.java
@@ -0,0 +1,43 @@
+package com.example.database;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class EventRepository {
+    private EventDao mEventDao;
+    private LiveData<List<Event>> mAllEvent;
+
+    EventRepository(Application application){
+        EventRoomDatabase db = EventRoomDatabase.getDatabase(application);
+        mEventDao = db.eventDao();
+        mAllEvent = mEventDao.getAllEvents();
+    }
+
+    LiveData<List<Event>> getAllEvent(){
+        return mAllEvent;
+    }
+
+    public void insert(Event event){
+        new insertAsyncTask(mEventDao).execute(event);
+    }
+
+    //LiveData<List<Event>> findEventByDate(String Date){return mEventDao.findEventByDate(Date);}
+    LiveData<List<Event>> findEventByDate(String Date){return mEventDao.findEventByDate(Date);}
+
+    private static class insertAsyncTask extends AsyncTask<Event,Void,Void> {
+        private EventDao mAsyncTaskDao;
+
+        insertAsyncTask(EventDao mEventDao) {
+            mAsyncTaskDao = mEventDao;
+        }
+
+        @Override
+        protected Void doInBackground(final Event... events) {
+            mAsyncTaskDao.insert(events[0]);
+            return null;
+        }
+    }
+}
