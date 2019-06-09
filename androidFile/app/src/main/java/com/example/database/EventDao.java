2626c0b0aa08037e1c7f8d4875131283bcc4a8d3
brianfung420 <brianfung420@gmail.com>
2019-06-09 14:07:49 +0800

    add EventDao

---
+++ androidFile/app/src/main/java/com/example/database/EventDao.java
@@ -0,0 +1,32 @@
+package com.example.database;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface EventDao {
+
+    @Insert                                                         //插入資料
+    void insert(Event... event);
+
+    //@Update                                                       //更新資料
+    //void updateEvent(int id);
+
+    @Query("DELETE FROM Event_table")                           //查詢資料
+    void deleteAll();
+
+    //@Query("SELECT Title & Time FROM Event_table WHERE Date = date")                           //查詢資料
+    //void findDetail(String date);
+
+
+    @Query("SELECT * FROM Event_table WHERE Date = :date")             //查詢資料
+    LiveData<List<Event>> findEventByDate(String date);
+
+    @Query("SELECT * FROM Event_table")
+    LiveData<List<Event>> getAllEvents();
+}
