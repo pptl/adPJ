6779cd91e01d0088e3ac244e928f95c2f35b385e
brianfung420 <brianfung420@gmail.com>
2019-06-11 23:46:15 +0800

    update profile recyclerView

--- androidFile/app/src/main/java/com/example/shuafeia/profile.java
+++ androidFile/app/src/main/java/com/example/shuafeia/profile.java
@@ -4,23 +4,24 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import com.example.Room1.Event;
 import com.example.Room1.EventViewModel;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class profile extends Fragment {
 
     private TextView Task_textView,Challange_textView,Reminded_textView;
     private EventViewModel mEventViewModel;
-
+    private RecyclerView recyclerView;
 
     @Nullable
     @Override
@@ -31,6 +32,13 @@ public class profile extends Fragment {
         Task_textView = RootView.findViewById(R.id.friend_task_done_count_textview);
         Challange_textView = RootView.findViewById(R.id.friend_challange_complete_count_textview);
         Reminded_textView = RootView.findViewById(R.id.friend_reminded_count_textview);
+
+        recyclerView = RootView.findViewById(R.id.profile_recyclerView);
+        final profileAdapter adapter = new profileAdapter(this.getContext());
+        DividerItemDecoration divider = new DividerItemDecoration(getActivity(),DividerItemDecoration.VERTICAL);
+        divider.setDrawable(ContextCompat.getDrawable(getActivity(),R.drawable.custom_divider));
+        recyclerView.addItemDecoration(divider);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         mEventViewModel = ViewModelProviders.of(this).get(EventViewModel.class);
         mEventViewModel.getAllEvent().observe(this, new Observer<List<Event>>() {
@@ -63,11 +71,12 @@ public class profile extends Fragment {
                 Task_textView.setText(Integer.toString(Task_Finish) + "/" + Integer.toString(Task_Number));
                 Challange_textView.setText(Integer.toString(Challange_Finish) + "/" + Integer.toString(Challange_Number));
                 Reminded_textView.setText(Integer.toString(Remind_Finish) + "/" + Integer.toString(Reminded_Number));
+                adapter.setEvent(events);
                 Log.d("Profile","Task Number:"+Task_Finish + "/"+Task_Number + " Challange:"+Challange_Finish+"/"+Challange_Number+" Remind:"+Remind_Finish+"/"+Reminded_Number);
             }
         });
 
-
+        recyclerView.setAdapter(adapter);
 
         return RootView;
     }
